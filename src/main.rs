d466505af1a66ca675ec06e45a3162af38ea5f2f
Ryan Rozanitis <ryan.rozanitis@gmail.com>
2023-02-11 23:47:33 -0800

    Revert "health_check and test case"
    This reverts commit c16a64d3e5014fdb1f8e932182942b929f1324ef.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-//! src/main.rs
-
-use std::net::TcpListener;
-use zero2prod::run;
-
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
-    //let port = listener.local_addr().unwrap().port();
-    run(listener)?.await
+fn main() {
+    println!("Hello, world!");
 }
